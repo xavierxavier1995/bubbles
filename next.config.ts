cbd802188f4bed8ddc4152980142b8519a941151
xavierxavier1995 <xavier@gabrielf.com>
2026-04-17 15:39:52 -0300

    feat(next.config): Allow remote images from bubbles.com.br
    Configures Next.js to allow images to be loaded from the specified remote domain, improving asset handling for external content.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.bubbles.com.br',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
